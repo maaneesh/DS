5d4864f0160f08c35fd0b213ace1361bfab998b0
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-09-18 18:35:42 -0400

    added main function to flow control

--- src/Conditionals/FlowControl.java
+++ src/Conditionals/FlowControl.java
@@ -579,5 +579,65 @@ public class FlowControl {
         }
     }
 
+    public static void main(String[] args) {
+        //1
+        // printNaturalNumbers();//2
+        // printToHundred();
+        // 3
+        // printOddNum();
+        //4
+        // evenSum();
+        //5
+        // oddSum();
+        //6
+        //  multiplicationTable();
+        //7
+        // countDigits();
+        //8
+        //sumOfDigits();
+        //9
+        // reverseNatural();
+        //10
+        // isPrime();
+        // System.out.println(isPrime(9));
+        //11
+        //listOfPrime();
+        //12
+        //   sumOfPrime();
+        //13
+//        isArmstrong(); ASK HOW TO DO IT? _________________________________???
+        //     printArmstrong();
+        //14
+        //listArmstrong();
+        // isPerfect();
+        // perfectList();
+
+//        System.out.println("Factorial: "+ factorial(1) );
+        //18
+//        strongList();
+        // swapFirstAndLast();
+        // swapNums(20,30);
+        // swapNumsII(20,30);
+        //22.
+        // powerFunc();
+        //23
+        // powerFuncII();
+        //24
+        // powerFuncIII();
+        // checkPalindrome();
+        //fibonacciFunctionII();
+        // frequencyOfDigits();
+        // asciiValues();
+//        alphabet();
+//        alphabetCapitalized();
+        //   factors();
+        //  HCF();
+//        findLCM();
+//primeFactors();
+
+
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -56,7 +56,7 @@ public class Main {
      //   factors();
       //  HCF();
 //        findLCM();
-primeFactors();
+//primeFactors();
 
 
 
